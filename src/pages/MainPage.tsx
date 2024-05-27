81bd322362718b0613e0679280a082205c90302a
NewBean0312 <newbean0312@gmail.com>
2024-05-27 10:15:07 +0900

    Display Section 작업

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,18 +1,22 @@
-import React from 'react'
-import Header from '../components/common/Header'
-import Slider from '../components/Slider'
+import React from "react";
+import Header from "../components/common/Header";
+import Slider from "../components/Slider";
+import Display from "../components/Display";
 
 import "../styles/Main.scss";
 import "../styles/Header.scss";
 import "../styles/MainContent.scss";
 
-const MainPage:React.FC = () => {
+const MainPage: React.FC = () => {
   return (
     <>
       <Header />
       <Slider />
+      <div className="content">
+        <Display />
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default MainPage
+export default MainPage;
